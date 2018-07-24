5ab3499ce93b03ec3ec799830e4bcd7cdf5eb858
potionsell <bscousin@mail-relay.iu.edu>
2018-07-24 10:30:26 -0400

    Added ability to compute blended line ratios. Fixed reddening coefficient computation.

--- lineRatios.py
+++ lineRatios.py
@@ -2,7 +2,6 @@ import pandas as pd
 import numpy as np
 
 def reddeningCoeff(objDF, lineDF):
-    pass
 
     '''
     intrRatio   ratio between the intrinsic flux of the chosen line and 
@@ -10,6 +9,7 @@ def reddeningCoeff(objDF, lineDF):
     curveDiffBB    difference in reddening curve flux between the wavelengths of 
                 the chosen line and reference Hbeta line
     '''
+    pass
 
 objDF = pd.read_csv('globalData.txt', sep='\t', index_col=0)
 lineDF = pd.read_csv('lineData.txt', sep='\t', index_col=0)
@@ -36,9 +36,13 @@ def findLineCurveDiff(line):
     curLine = lineID_DF.iloc[ID_idx]
     return float(curLine['redCurve'])
 
-def computeRatio(line1,line2):
+def computeRatio(line1,line2,line3=None):
     line1Flux = findLineFlux(line1)
-    line2Flux = findLineFlux(line2)
+    if line3 != None:
+        sumFlux = findLineFlux(line2) + findLineFlux(line3)
+        line2Flux = sumFlux
+    else:
+        line2Flux = findLineFlux(line2)
     obsRatio = line2Flux / line1Flux
     
     curveDiffB1 = findLineCurveDiff(line1)
@@ -47,7 +51,9 @@ def computeRatio(line1,line2):
     corrRatio = obsRatio * 10**(redCoeff * (curveDiffB2-curveDiffB1) )
     return corrRatio
 
-for i in objIDs:
+testIDs = ['1627','217','Dot']
+for i in testIDs:
+#for i in objIDs:
     #skip if the object has only 1 line (which triggers errors if I don't skip it)
     if objDF.loc[i]['nLines'] == 1: continue
     
@@ -58,12 +64,14 @@ for i in objIDs:
         line = Halpha
         obsFlux = findLineFlux(line)
         intrRatio = 2.86        #Halpha:Hbeta
+        flag = 1
             
     #OIII detected
     elif Hgamma in objLines['lineID'].values and Hbeta in objLines['lineID'].values:
         line = Hgamma
         obsFlux = findLineFlux(line)
         intrRatio = 0.47        #Hgamma:Hbeta
+        flag = 2
     else: continue #cry me a river
 
     #look up values in line ID table
@@ -75,16 +83,19 @@ for i in objIDs:
     redCoeff = reddeningCorr(obsRatio, intrRatio, curveDiffB)
 
     objDF.loc[i, 'redCoeff'] = redCoeff
-
+#    objDF.loc[i, 'redFlag'] = flag
     
     ##Now compute line ratios
     Hbeta = 4861
     Halpha = 6563
     Hgamma = 4340
-    NII = 6548
-    SII = 4072
+    NII = 6584
+    SII_1 = 6717
+    SII_2 = 6731
+    OII_1 = 3726
+    OII_2 = 3729
     OIII = 5007
-    OII = 3727      #this is blended - need  to account for the rough flux!!
+    
     NeIII = 3869
     if Halpha in objLines['lineID'].values:
         line1 = Halpha
@@ -92,10 +103,11 @@ for i in objIDs:
             line2 = NII
             corrRatio = computeRatio(line1,line2)
             objDF.loc[i, 'NII/Ha'] = corrRatio
-            
-        if SII in objLines['lineID'].values:
-            line2 = SII
-            corrRatio = computeRatio(line1,line2)
+#            import pdb; pdb.set_trace()
+        if SII_1 in objLines['lineID'].values and SII_2 in objLines['lineID'].values:
+            line2 = SII_1
+            line3 = SII_2
+            corrRatio = computeRatio(line1,line2,line3)
             objDF.loc[i, 'SII/Ha'] = corrRatio
             
     if Hbeta in objLines['lineID'].values:
@@ -105,9 +117,10 @@ for i in objIDs:
             corrRatio = computeRatio(line1,line2)
             objDF.loc[i, 'OIII/Hb'] = corrRatio
             
-        if OII in objLines['lineID'].values:
-            line2 = OII
-            corrRatio = computeRatio(line1,line2)
+        if OII_1 in objLines['lineID'].values and OII_2 in objLines['lineID'].values:
+            line2 = OII_1
+            line3 = OII_2
+            corrRatio = computeRatio(line1,line2,line3)
             objDF.loc[i, 'OII/Hb'] = corrRatio
                         
         if NeIII in objLines['lineID'].values:
@@ -115,12 +128,15 @@ for i in objIDs:
             corrRatio = computeRatio(line1,line2)
             objDF.loc[i, 'NeIII/Hb'] = corrRatio
     
-    if OII in objLines['lineID'].values and NeIII in objLines['lineID'].values:
-        line1 = OII
-        line2 = NeIII
-        corrRatio = computeRatio(line1,line2)
+    if NeIII in objLines['lineID'].values and OII_1 in objLines['lineID'].values and OII_2 in objLines['lineID'].values:
+        #line1 = OII
+        #line2 = NeIII
+        line1 = NeIII
+        line2 = OII_1
+        line3 = OII_2
+        corrRatio = 1. /computeRatio(line1,line2)
         objDF.loc[i, 'NeIII/OII'] = corrRatio
-#    import pdb; pdb.set_trace()
+    import pdb; pdb.set_trace()
 
         
 def lineRatios(objDF, lineDF):
